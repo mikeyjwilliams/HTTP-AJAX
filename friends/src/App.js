bde6ba480e177efe467a4ed03fa139119d069bdd
Michael Williams <manicdesigner@gmail.com>
2019-06-25 18:57:36 -0400

    feat(App.js) + imports axios & Friends + state friend and if < 0 loading appears

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import Friends from './components/Friends';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: null
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Friends List</h1>
+        {this.state.friends > 0 ? <Friends /> : <h2>Loading data....</h2>}
+      </div>
+    );
+  }
 }
 
 export default App;
