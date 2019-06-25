df53839c7f149e24ae30b77f2e1d14891a7d19cf
Michael Williams <manicdesigner@gmail.com>
2019-06-25 19:17:00 -0400

    component to list friends from server

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Friends = props => {
   return (
     <div>
-      <h2>Friend</h2>
+      <h2>props.name</h2>
+      <h3>props.age</h3>
+      <h3>props.email</h3>
     </div>
   );
 };
