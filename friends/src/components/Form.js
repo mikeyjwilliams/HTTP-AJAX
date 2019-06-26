358be8ce5be71d77f3e0a61bb0f0da3b87f9ff9d
Michael Williams <manicdesigner@gmail.com>
2019-06-25 20:44:39 -0400

    feat(Form.js) starting form

---
+++ friends/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+export default Form;
