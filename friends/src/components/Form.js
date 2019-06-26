a7db7d14754ac7143321a4c10e189db8e189d88b
Michael Williams <manicdesigner@gmail.com>
2019-06-26 14:08:49 -0400

    feat(Form.js) + add some state to mod + changeHandler to hodl value + update first input name & onChange event handler added

--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -3,12 +3,22 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: '',
+      age: 0,
+      email: '',
+      friend: []
+    };
   }
+
+  changeHandler = e => {
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <form>
-        <input type="text" />
+        <input type="text" name="name" onChange={this.changeHandler} />
       </form>
     );
   }
