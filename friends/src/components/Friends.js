c640cf4fb9468ecb7748845590cd0001f2999ad6
Michael Williams <manicdesigner@gmail.com>
2019-06-25 20:45:43 -0400

    styles(Friends.js) + class for styling

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Friends = props => {
   return (
-    <div>
-      <h2>{props.friend.name}</h2>
-      <h3>{props.friend.age}</h3>
-      <h3>{props.friend.email}</h3>
+    <div className="friends-row">
+      <h2 className="center-vert">{props.friend.name}</h2>
+      <div>
+        <h3>{props.friend.age}</h3>
+        <h3>{props.friend.email}</h3>
+      </div>
     </div>
   );
 };
