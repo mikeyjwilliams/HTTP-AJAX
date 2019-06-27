a31a32a1aeaa4067b93d453b3fbedcffd5f68e07
Michael Williams <manicdesigner@gmail.com>
2019-06-27 17:55:20 -0400

    feat(App.js) + addItem handler + add to Friends component

--- friends/src/App.js
+++ friends/src/App.js
@@ -17,14 +17,19 @@ class App extends React.Component {
     axios
       .get(`http://localhost:5000/friends`)
       .then(response => {
-        this.setState({
-          friends: response.data
-        });
+        this.setState({ friends: response.data });
       })
       .catch(err => {
         console.log(err);
       });
   }
+
+  addItem = friend => {
+    axios
+      .post(`http://localhost:5000/friends`, friend)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
 
   render() {
     return (
@@ -33,7 +38,9 @@ class App extends React.Component {
         <Form />
         {this.state.friends.length > 0 ? (
           this.state.friends.map(friend => {
-            return <Friends key={friend.id} friend={friend} />;
+            return (
+              <Friends key={friend.id} friend={friend} addItem={this.addItem} />
+            );
           })
         ) : (
           <h2>Loading data....</h2>
