64481138160d5dc200175a0d9fb912ff2b4240a2
Michael Williams <manicdesigner@gmail.com>
2019-06-27 20:49:16 -0400

    update to FriendsList.js

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Friends from './Friends';
 
-export default function FriendList(props) {
+export default function FriendsList(props) {
   return (
     <div>
       {props.friends.length > 0 ? (
         props.friends.map(friend => {
           return (
-            <Friends
-              key={friend.id}
-              friends={friends}
-              addItem={props.addItem}
-            />
+            <Friends key={friend.id} friend={friend} addItem={props.addItem} />
           );
         })
       ) : (
