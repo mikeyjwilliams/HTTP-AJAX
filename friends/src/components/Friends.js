a60e6159ef9752154c210af9c3471457fd240692
Michael Williams <manicdesigner@gmail.com>
2019-06-27 21:36:50 -0400

    got rid of excessive styles feat(Friends) + buttons for save and delete in progress

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -3,35 +3,12 @@ import PropTypes from 'prop-types';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-//import Icon from '@material-ui/core/Icon';
-//import CardActions from '@material-ui/core/CardActions';
-//import Button from '@material-ui/core/Button';
+
 import Typography from '@material-ui/core/Typography';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  card: {
-    mainWidth: 275
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  title: {
-    fontSize: 18
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
-
 const Friends = props => {
-  const jsClasses = useStyles();
-
   return (
-    <Card className={`${jsClasses.card} mdc-card`}>
+    <Card className={` mdc-card`}>
       <CardContent>
         <div className="friends-row">
           <div className="center-vert">
@@ -46,6 +23,10 @@ const Friends = props => {
             <Typography component="h4" variant="h5">
               {props.friend.email}
             </Typography>
+            <button type="button" onClick={props.saveFriend}>
+              Save
+            </button>
+            <button type="button">X</button>
           </div>
         </div>
       </CardContent>
@@ -53,11 +34,11 @@ const Friends = props => {
   );
 };
 
-Friends.propTypes = {
-  friend: PropTypes.shape({
-    name: PropTypes.string,
-    age: PropTypes.number,
-    email: PropTypes.string
-  })
-};
+// Friends.propTypes = {
+//   friend: PropTypes.shape({
+//     name: PropTypes.string,
+//     age: PropTypes.number,
+//     email: PropTypes.string
+//   })
+// };
 export default Friends;
