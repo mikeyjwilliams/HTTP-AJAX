629c0c0cae945b78950401fb3044872e19ea4a5b
Michael Williams <manicdesigner@gmail.com>
2019-06-30 21:53:40 -0400

    feat(App.js) + createFriend compoent

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { NavLink, Link, Route } from 'react-router-dom';
-
-import './App.css';
 import FriendsList from './components/FriendsList';
+import CreateFriend from './components/CreateFriend';
+import './App.css';
 
 class App extends React.Component {
   constructor() {
@@ -29,13 +29,44 @@ class App extends React.Component {
     }
   }
 
+  updateFriendsList = friends => {
+    this.setState({ friends });
+    console.log('AFTER update', this.state.friends);
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="nav-section center-text">Friends</div>
-        <header className="App-header" />
-        <h2>Friends List</h2>
-        <FriendsList friends={this.state.friends} className="center-text" />
+      <div>
+        <div className="App">
+          <nav>
+            <Link to="/" className="nav">
+              Friends
+            </Link>
+            <Link to="/create" className="nav">
+              Create Friend
+            </Link>
+          </nav>
+          <div className="nav-section center-text">Friends</div>
+          <header className="App-header" />
+          {/* <FriendsList friends={this.state.friends} className="center-text" /> */}
+        </div>
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <FriendsList {...props} friends={this.state.friends} />
+          )}
+        />
+        <Route
+          path="/create"
+          exact
+          render={props => (
+            <CreateFriend
+              {...props}
+              updateFriendsList={this.updateFriendsList}
+            />
+          )}
+        />
       </div>
     );
   }
