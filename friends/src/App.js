094d3c6acd0387f112ff721f894497f53aa65d18
Michael Williams <manicdesigner@gmail.com>
2019-06-30 20:39:24 -0400

    feat(App.js) + get request + display FriendList component w/ friends data on it

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import { NavLink, Link, Route } from 'react-router-dom';
 
 import './App.css';
+import FriendsList from './components/FriendsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Friends List</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(res => {
+        this.setState({ friends: res.data });
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    if (!this.state.friends) {
+      this.setState({ friends: this.state.friends });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="nav-section">Friends</div>
+        <header className="App-header" />
+        <h2>Friends List</h2>
+        <FriendsList friends={this.state.friends} />
+      </div>
+    );
+  }
 }
-
 export default App;
