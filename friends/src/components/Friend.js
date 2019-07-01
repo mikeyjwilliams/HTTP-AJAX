7fd5eaed1b1b7fbdddbfe74fec07db211ea02de3
Michael Williams <manicdesigner@gmail.com>
2019-06-30 20:13:57 -0400

    feat(Friend.js) Friend compoent to display friend data

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Friend(props) {
+  console.log('Friend ', props.friend);
+  return (
+    <div>
+      <h3>Friend item</h3>
+    </div>
+  );
+}
