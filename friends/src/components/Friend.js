31749e0018442e72a07926190f980512d7ba6cfa
Michael Williams <manicdesigner@gmail.com>
2019-06-30 20:38:28 -0400

    feat(Friend.js) + display friend info

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Friend(props) {
   console.log('Friend ', props.friend);
   return (
-    <div>
-      <h3>Friend item</h3>
+    <div className="info-section">
+      <h3>{props.friend.name}</h3>
+      <div className="friend-info-section">
+        <p className="friend-age">{props.friend.age}</p>
+        <p className="friend-email">{props.friend.email}</p>
+      </div>
     </div>
   );
 }
