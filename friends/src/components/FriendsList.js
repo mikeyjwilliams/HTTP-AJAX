85c783d0723af808ead32c98c41d159e4c8c96e3
Michael Williams <manicdesigner@gmail.com>
2019-06-30 20:02:41 -0400

    test component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FriendsList(props) {
+  console.log('Friend data', props.friends);
+  return (
+    <div>
+      <h2>Friends list</h2>
+    </div>
+  );
+}
