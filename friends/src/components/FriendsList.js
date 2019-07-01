827f38be7d018f79472f8bfe40a7e9d45ff6570b
Michael Williams <manicdesigner@gmail.com>
2019-06-30 20:35:14 -0400

    feat(FriendsList.js) + map friend component + or loading data sign else wise

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -5,9 +5,13 @@ export default function FriendsList(props) {
   console.log('Friend data', props.friends);
   return (
     <div>
-      {props.friends.map(friend => {
-        return <Friend key={friend.id} friend={friend} />;
-      })}
+      {props.friends.length > 0 ? (
+        props.friends.map(friend => {
+          return <Friend key={friend.id} friend={friend} />;
+        })
+      ) : (
+        <h2>Loading Data...</h2>
+      )}
     </div>
   );
 }
