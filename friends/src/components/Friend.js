415ce604b91a461f0e2d95725459e281c19d2e62
Michael Williams <manicdesigner@gmail.com>
2019-07-05 16:14:42 -0400

    feat(Friend.js) + add fontawesome + mod div to section around area + have edit icon for update-section

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Friend.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+
+const edit = <FontAwesomeIcon icon={faEdit} size="2x" />;
+
 export default function Friend(props) {
   return (
-    <div className="info-section">
+    <section className="info-section">
       <div className="vert-center">
         <h3>{props.friend.name}</h3>
       </div>
@@ -11,6 +16,9 @@ export default function Friend(props) {
         <p className="friend-age">{props.friend.age}</p>
         <p className="friend-email">{props.friend.email}</p>
       </div>
-    </div>
+      <div className="update-section">
+        <div>{edit}</div>
+      </div>
+    </section>
   );
 }
