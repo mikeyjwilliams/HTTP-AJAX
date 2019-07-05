813ce23b2b69ae456e3b20baf0f22c876f2cfd6e
Michael Williams <manicdesigner@gmail.com>
2019-07-05 17:11:16 -0400

    test(updateuser.js) + test for data coming through + get for CDM

--- friends/src/components/UpdateUser.js
+++ friends/src/components/UpdateUser.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 class UpdateUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-           name: '',
-           age: '',
-           email: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      email: ''
+    };
+  }
+  componentDidMount() {
+    const findFriend = this.props.match.params.id;
 
-        }
-    }
-    componentDidMount() {
-       
-    }
+    axios
+      .get(`http:/localhost:3000/friend/${findFriend}`)
+      .then(response => {
+        const { name, age, email } = response.data;
+        this.setState({ name, age, email });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
+  render() {
+    console.log(this.state);
+    const { name, age, email } = this.state;
+    return (
+      <div>
+        <h2>{name}</h2>
+        <h3>{age}</h3>
+        <h3>{email}</h3>
+      </div>
+    );
+  }
 }
 export default UpdateUser;
