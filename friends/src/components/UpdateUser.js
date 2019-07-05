20f92a3dadd4e2c0bd7e34e16188147d41144a4c
Michael Williams <manicdesigner@gmail.com>
2019-07-05 15:25:19 -0400

    working on component UpdateUser.js

---
+++ friends/src/components/UpdateUser.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class UpdateUser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+           name: '',
+           age: '',
+           email: '',
+
+        }
+    }
+    componentDidMount() {
+       
+    }
+
+}
+export default UpdateUser;
