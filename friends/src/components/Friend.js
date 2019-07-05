8d7353a58c5aee4c35120e886151a942b9960086
Michael Williams <manicdesigner@gmail.com>
2019-07-05 15:24:29 -0400

    feat(Friend.js) + import for style + div for centering vert name prop

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Friend.css';
 
 export default function Friend(props) {
-  console.log('Friend ', props.friend);
   return (
     <div className="info-section">
-      <h3>{props.friend.name}</h3>
+      <div className="vert-center">
+        <h3>{props.friend.name}</h3>
+      </div>
       <div className="friend-info-section">
         <p className="friend-age">{props.friend.age}</p>
         <p className="friend-email">{props.friend.email}</p>
